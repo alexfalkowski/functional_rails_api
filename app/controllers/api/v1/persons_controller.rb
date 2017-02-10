20886a1bf7fce2c8ba2fd7d391af936122d247cd
Alex Falkowski <alexrfalkowski@gmail.com>
2017-02-10 20:28:47 +0800

    Upgraded rfunk (1.7.0)

--- app/controllers/api/v1/persons_controller.rb
+++ app/controllers/api/v1/persons_controller.rb
@@ -1,13 +1,12 @@
 module Api
   module V1
     class PersonsController < ApplicationController
+      include RFunk::Match
+
       def create
-        either = RFunk.either(-> { Person.create!(person_attributes) })
-        case either
-          when RFunk::Success
-            render json: { message: either.result }, status: :created
-          when RFunk::Failure
-            render json: { error: either.result }, status: :unprocessable_entity
+        match(RFunk.either(-> { Person.create!(person_attributes) })) do |p|
+          p.with :success, ->(v) { render json: { message: v }, status: :created }
+          p.with :failure, ->(v) { render json: { error: v }, status: :unprocessable_entity }
         end
       end
 
